1a0568c0a1988bf8b18e43e2d676806a2575382e
Andrade <vinicius_andrade2010@hotmail.com>
2024-01-07 17:20:16 -0300

    feat: LeetCode 35 - SearchInsertPosition

--- 4_arrays/_35_SearchInsertPosition.java
+++ 4_arrays/_35_SearchInsertPosition.java
@@ -51,7 +51,6 @@ public class _35_SearchInsertPosition {
     public static int searchInsert(int[] nums, int target) {
         if (nums == null || nums.length == 0 || target < nums[0]) return 0;
 
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target || nums[i] > target) return i;
         }
