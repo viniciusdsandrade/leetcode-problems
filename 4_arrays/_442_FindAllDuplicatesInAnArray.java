7151d899fb047b489821436489370ea49e4852c4
Andrade <vinicius_andrade2010@hotmail.com>
2024-01-09 00:28:55 -0300

    feat: LeetCode 189 - RotateArray

--- 4_arrays/_442_FindAllDuplicatesInAnArray.java
+++ 4_arrays/_442_FindAllDuplicatesInAnArray.java
@@ -2,6 +2,33 @@ import java.util.*;
 
 public class _442_FindAllDuplicatesInAnArray {
 
+    /*
+    442. Find All Duplicates in an Array
+    Given an integer array nums of length n where all the integers of nums
+    are in the range [1, n] and each integer appears once or twice,
+    return an array of all the integers that appears twice.
+
+    You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+    Example 1:
+    Input: nums = [4,3,2,7,8,2,3,1]
+    Output: [2,3]
+
+    Example 2:
+    Input: nums = [1,1,2]
+    Output: [1]
+
+    Example 3:
+    Input: nums = [1]
+    Output: []
+
+    Constraints:
+    n == nums.length
+    1 <= n <= 105
+    1 <= nums[i] <= n
+    Each element in nums appears once or twice.
+     */
+
     public static void main(String[] args) {
         int[] nums1 = {4, 3, 2, 7, 8, 2, 3, 1};
         testFindDuplicates(nums1);
@@ -17,6 +44,7 @@ public class _442_FindAllDuplicatesInAnArray {
     }
 
     public static List<Integer> findDuplicates(int[] nums) {
+        // Caso o array seja nulo ou vazio, retorna nulo
         if (nums == null || nums.length == 0) return null;
 
         List<Integer> result = new ArrayList<>();
