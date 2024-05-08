12883d0c58b870ed50f3478f5c7bc9a5a9b9a2ab
Vinicius <vinicius_andrade2010@hotmail.com>
2024-05-08 13:35:36 -0300

    feat: _1512_Number_of_Good_Pairs.py

--- Python/_4_arrays/_55_Jump_Game.py
+++ Python/_4_arrays/_55_Jump_Game.py
@@ -31,7 +31,8 @@ class Solution:
         for i in range(n):
             if i > max_reachable: return False
             max_reachable = max(max_reachable, i + nums[i])
-            if max_reachable >= n - 1: return True
+            if max_reachable >= n - 1:
+                return True
         return False
 
     @staticmethod
